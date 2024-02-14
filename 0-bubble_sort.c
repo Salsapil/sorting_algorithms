8539390778da2c363b5d106543d9fe5eb0129c62
Salsabil <salsabeelahmed323@gmail.com>
2024-02-15 00:22:53 +0200

    Sorting Algorithm_ Task0-Task1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers using Bubble sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j, temp;
+int flag;
+if (!array || !size)
+{
+return;
+}
+for (i = 0; i < size; i++)
+{
+flag = 0;
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+flag = 1;
+}
+}
+if (flag == 0)
+{
+break;
+}
+}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+/**
+ * swap - swap tow nodes in DLL
+ * @first_node: first node to swap it with second node
+ * @second_node: second node to swap it with first node
+ * Return: void
+ */
+void swap(listint_t *first_node, listint_t *second_node)
+{
+if (first_node->prev != NULL)
+{
+first_node->prev->next = second_node;
+}
+if (second_node->next != NULL)
+{
+second_node->next->prev = first_node;
+}
+second_node->prev = first_node->prev;
+first_node->next = second_node->next;
+second_node->next = first_node;
+first_node->prev = second_node;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list.
+ * @list: Pointer to the head of the doubly linked list.
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *i, *j;
+if (list == NULL || !(*list))
+return;
+
+i = (*list)->next;
+while (i != NULL)
+{
+j = i;
+while (j && j->prev)
+{
+if (j->n < j->prev->n)
+{
+swap(j->prev, j);
+if (!j->prev)
+{
+*list = j;
+}
+print_list((const listint_t *)*list);
+}
+else
+{
+j = j->prev;
+}
+}
+i = i->next;
+}
+}
