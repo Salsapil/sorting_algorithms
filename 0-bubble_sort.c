bbbea700f09a8d082f270612b83e067f3f088bfb
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-17 12:07:45 +0200

    formating

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,29 +7,29 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j, temp;
-int flag;
-if (!array || !size)
-{
-return;
-}
-for (i = 0; i < size; i++)
-{
-flag = 0;
-for (j = 0; j < size - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-print_array(array, size);
-flag = 1;
-}
-}
-if (flag == 0)
-{
-break;
-}
-}
+	size_t i, j, temp;
+	int flag;
+	if (!array || !size)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			break;
+		}
+	}
 }
