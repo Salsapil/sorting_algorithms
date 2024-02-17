a74ebb71d0aa742654f075fc9bf25e2aada1f17e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-17 12:28:04 +0200

    betty fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,18 +7,18 @@
  */
 void swap(listint_t *first_node, listint_t *second_node)
 {
-if (first_node->prev != NULL)
-{
-first_node->prev->next = second_node;
-}
-if (second_node->next != NULL)
-{
-second_node->next->prev = first_node;
-}
-second_node->prev = first_node->prev;
-first_node->next = second_node->next;
-second_node->next = first_node;
-first_node->prev = second_node;
+	if (first_node->prev != NULL)
+	{
+		first_node->prev->next = second_node;
+	}
+	if (second_node->next != NULL)
+	{
+		second_node->next->prev = first_node;
+	}
+	second_node->prev = first_node->prev;
+	first_node->next = second_node->next;
+	second_node->next = first_node;
+	first_node->prev = second_node;
 }
 
 /**
@@ -28,30 +28,31 @@ first_node->prev = second_node;
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *i, *j;
-if (list == NULL || !(*list))
-return;
+	listint_t *i, *j;
+	
+	if (list == NULL || !(*list))
+		return;
 
-i = (*list)->next;
-while (i != NULL)
-{
-j = i;
-while (j && j->prev)
-{
-if (j->n < j->prev->n)
-{
-swap(j->prev, j);
-if (!j->prev)
-{
-*list = j;
-}
-print_list((const listint_t *)*list);
-}
-else
-{
-j = j->prev;
-}
-}
-i = i->next;
-}
+	i = (*list)->next;
+	while (i != NULL)
+	{
+		j = i;
+		while (j && j->prev)
+		{
+			if (j->n < j->prev->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+				{
+					*list = j;
+				}
+				print_list((const listint_t *)*list);
+			}
+			else
+			{
+				j = j->prev;
+			}
+		}
+		i = i->next;
+	}
 }
